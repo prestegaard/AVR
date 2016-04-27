fa84f635e673a665dfa3e2b9d7d62f38b5fb959b
prestegaard <Ikstart2>
2016-04-27 12:21:19 +0200

    Made pwm drivers
    XMEGA-E5_prototype

---
+++ RC_CAR/XMEGA_E5/pwm.h
@@ -0,0 +1,17 @@
+/*
+ * pwm.h
+ *
+ * Created: 27.04.2016 00.11.25
+ *  Author: Haagon
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+void pwm_init();
+void pwm_set(uint8_t wheel, uint8_t speed);
+
+
+
+#endif /* PWM_H_ */
